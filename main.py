cb193a32a85af9df0e0aa334724c5f3b055ce4f0
Yana Sebrukovich <85887443+YaninaSeb@users.noreply.github.com>
2023-05-04 23:37:09 +0300

    feat: add task2 solution

--- main.py
+++ main.py
@@ -7,3 +7,14 @@
 #   if letter in ['a', 'e', 'i', 'o', 'u', 'y']:
 #     count += 1
 # print('number of vowels in a string is', count, '\n')
+
+# # TASK 2
+# #Найти сумму цифр числа
+# num = input('Please, enter the number\n')
+# num = abs(int(num))
+# sum = 0
+# while num > 10:
+#   sum += num % 10
+#   num //= 10
+# sum += num
+# print('The sum of the digits of a number', sum, '\n')
