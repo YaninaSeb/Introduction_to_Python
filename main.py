eadccda4be38a588ada76e488082b03734b13884
Yana Sebrukovich <85887443+YaninaSeb@users.noreply.github.com>
2023-05-04 23:35:51 +0300

    feat: add solution

---
+++ main.py
@@ -0,0 +1,9 @@
+# # TASK 1
+# # Найти количество гласных букв в строке введенной пользователем
+# str = input('Please, enter the string\n')
+# str = str.lower()
+# count = 0
+# for letter in str:
+#   if letter in ['a', 'e', 'i', 'o', 'u', 'y']:
+#     count += 1
+# print('number of vowels in a string is', count, '\n')
