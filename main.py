d89f0a883738be2998926a49a9d1df6b574f3266
Yana Sebrukovich <85887443+YaninaSeb@users.noreply.github.com>
2023-05-10 23:25:14 +0300

    refactor: update task11 solution

--- main.py
+++ main.py
@@ -100,21 +100,27 @@
 
 # # TASK 11
 # #Бактерии бывают двух видов - черные и белые. Черная бактерия за каждый такт делится на 2 черные и 1 белую, белая - на 1 черную и 1 белую. Выяснить сколько бактерий каждого вида будет после T тактов, если на первом такте было А - белых и В - черных .
-# countWhite = int(input('Please, enter the number of white bacteria: '))
-# countBlack = int(input('Please, enter the number of black bacteria: '))
-# countTakt = int(input('Please, enter the number of takt: '))
+# try:
+#   countWhite = int(input('Please, enter the number of white bacteria: '))
+#   countBlack = int(input('Please, enter the number of black bacteria: '))
+#   countTact = int(input('Please, enter the number of tact: '))
 
-# def changeCount(countW, countB, takt):
-#   if takt == countTakt:
-#     return countW, countB
-#   else:
-#     newCountW = countW + countB
-#     newCountB = countW + countB * 2
-#     takt += 1
-#     return changeCount(newCountW, newCountB, takt)
+#   if countWhite < 0 or countBlack < 0 or countTact <= 0: raise ValueError
 
-# countWhite, countBlack = changeCount(countWhite, countBlack, 1)
-# print('After', countTakt, "cycles the number of white bacteria is", countWhite, ',the number of black bacteria is', countBlack)
+#   def changeCount(countW, countB, tact):
+#     if tact == countTact:
+#       return countW, countB
+#     else:
+#       newCountW = countW + countB
+#       newCountB = countW + countB * 2
+#       tact += 1
+#       return changeCount(newCountW, newCountB, tact)
+
+#   countWhite, countBlack = changeCount(countWhite, countBlack, 1)
+#   print('After', countTact, "cycles the number of white bacteria is", countWhite, 'and the number of black bacteria is', countBlack)
+
+# except ValueError:
+#   print('The entered data is incorrect!')
 
 
 # # TASK 12
